114c6109a54fd303eeeb675688df92369e50d556
Mathias Bynens <mathias@qiwi.be>
2014-04-10 11:23:41 -0700

    Tag the v0.2.0 release

--- at.js
+++ at.js
@@ -1,4 +1,4 @@
-/*! http://mths.be/at v0.1.0 by @mathias */
+/*! http://mths.be/at v0.2.0 by @mathias */
 if (!String.prototype.at) {
 	(function() {
 		'use strict'; // needed to support `apply`/`call` with `undefined`/`null`
